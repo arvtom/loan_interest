81d6d1c39e765fadd334b03d816307fc14d13964
arvtom <tomkusarvydas@gmail.com>
2024-01-06 14:54:57 +0000

    plot from my cpp script works

--- loan_interest.cpp
+++ loan_interest.cpp
@@ -1,6 +1,7 @@
 #include "loan_interest.hpp"
 
-void demo_png() {
+void demo_png() 
+{
     Gnuplot gp;
 
     gp << "set terminal png\n";
@@ -36,40 +37,7 @@ void demo_png() {
     gp.send1d(xy_pts_B);
 }
 
-int main(int argc, char **argv) {
-    std::map<std::string, void (*)(void)> demos;
-
-    // demos["basic"]                  = demo_basic;
-    // demos["binary"]                 = demo_binary;
-    // demos["tmpfile"]                = demo_tmpfile;
-    demos["png"]                    = demo_png;
-    // demos["vectors"]                = demo_vectors;
-    // demos["script_inline_text"]     = demo_inline_text;
-    // demos["script_inline_binary"]   = demo_inline_binary;
-    // demos["script_external_text"]   = demo_external_text;
-    // demos["script_external_binary"] = demo_external_binary;
-    // demos["animation"]              = demo_animation;
-    // demos["nan"]                    = demo_NaN;
-    // demos["segments"]               = demo_segments;
-    // demos["image"]                  = demo_image;
-    // demos["plotgroup"]              = demo_plotgroup;
-    // demos["fit"]                    = demo_fit;
-
-    if(argc < 2) {
-        printf("Usage: %s <demo_name>\n", argv[0]);
-        printf("Choose one of the following demos:\n");
-        typedef std::pair<std::string, void (*)(void)> demo_pair;
-        for(const demo_pair &pair : demos) {
-            printf("    %s\n", pair.first.c_str());
-        }
-        return 0;
-    }
-
-    std::string arg(argv[1]);
-    if(!demos.count(arg)) {
-        printf("No such demo '%s'\n", arg.c_str());
-        return 1;
-    }
-
-    demos[arg]();
+int main() 
+{
+    demo_png();
 }
