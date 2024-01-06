669a38a5e0e364087d7a4e63c699eea30fe03df0
arvtom <tomkusarvydas@gmail.com>
2024-01-06 14:35:23 +0000

    attempt to use gnuplot

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get -y install git g++ python3 pip nano
+apt-get -y install git g++ python3 pip nano gnuplot libgnuplot-iostream-dev
 pip install matplotlib numpy
 
 #Get newest version of matplotlib-cpp
@@ -7,3 +7,6 @@ cd /home
 git clone https://github.com/lava/matplotlib-cpp.git
 cd matplotlib-cpp
 
+cd /home && \
+git clone https://github.com/dstahlke/gnuplot-iostream.git && \
+cd gnuplot-iostream
