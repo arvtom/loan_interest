d68af6aeead8ef00b4af5c587997315921a86269
Arvydas Tomkus <145611063+agnebiel@users.noreply.github.com>
2024-01-06 14:11:37 +0000

    attempt to use matplotlib-cpp

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,9 @@
+apt-get update
+apt-get -y install git g++ python3 pip nano
+pip install matplotlib numpy
+
+#Get newest version of matplotlib-cpp
+cd /home
+git clone https://github.com/lava/matplotlib-cpp.git
+cd matplotlib-cpp
+
