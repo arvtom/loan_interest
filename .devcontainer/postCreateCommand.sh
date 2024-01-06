3e58d737700740631b1d67f0f37272b0d2ecc34d
arvtom <tomkusarvydas@gmail.com>
2024-01-06 14:38:02 +0000

    gnuplot

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,7 +2,10 @@ git config --global user.email "tomkusarvydas@gmail.com"
 git config --global user.name "arvtom"
 
 apt-get update
-apt-get -y install git g++ python3 pip nano gnuplot libgnuplot-iostream-dev
+apt-get -y install \
+    git g++ nano\
+    python3 pip \
+    gnuplot libgnuplot-iostream-dev libboost-all-dev
 pip install matplotlib numpy
 
 #Get newest version of matplotlib-cpp
