624f0a694b4de2d4b566aa92fb6afaed819bd115
arvtom <tomkusarvydas@gmail.com>
2024-01-06 19:54:12 +0000

    Count number of days between two dates using calendar lib.

--- loan_interest.cpp
+++ loan_interest.cpp
@@ -5,15 +5,33 @@ int main()
     // demo_png();
 
     using namespace std::chrono;
-    const auto today = sys_days{std::chrono::floor<days>(system_clock::now())};
-    for (const year_month_day ymd : {{November/15/2020}, {November/15/2120}, today})
-    {
-        // std::cout << ymd;
-        const auto delta = (sys_days{ymd} - today).count();
-        (delta < 0) ? std::cout << " was " << -delta << " day(s) ago\n" :
-        (delta > 0) ? std::cout << " is " << delta << " day(s) from now\n"
-                    : std::cout << " is today!\n";
-    }
+
+    const std::chrono::time_point now{std::chrono::system_clock::now()};
+    std::chrono::year_month_day ymd_0{std::chrono::floor<std::chrono::days>(now)};
+    std::chrono::year_month_day ymd_1{std::chrono::floor<std::chrono::days>(now)};
+
+    ymd_1 += std::chrono::years(30);
+
+    std::cout << "Current Year: " << static_cast<int>(ymd_0.year()) << ", "
+                "Month: " << static_cast<unsigned>(ymd_0.month()) << ", "
+                "Day: " << static_cast<unsigned>(ymd_0.day()) << "\n";
+
+    std::cout << "Current Year: " << static_cast<int>(ymd_1.year()) << ", "
+                "Month: " << static_cast<unsigned>(ymd_1.month()) << ", "
+                "Day: " << static_cast<unsigned>(ymd_1.day()) << "\n";
+
+    // const auto today = sys_days{std::chrono::floor<days>(system_clock::now())};
+    // for (const year_month_day ymd : {{November/15/2020}, {November/15/2120}, today})
+    // {
+    //     // std::cout << ymd;
+    //     const auto delta = (sys_days{ymd} - today).count();
+    //     (delta < 0) ? std::cout << " was " << -delta << " day(s) ago\n" :
+    //     (delta > 0) ? std::cout << " is " << delta << " day(s) from now\n"
+    //                 : std::cout << " is today!\n";
+    // }
+
+    const auto delta = (sys_days{ymd_1} - sys_days{ymd_0}).count();
+    std::cout << "delta=" << delta << "\n";
 }
 
 // void demo_png() 
